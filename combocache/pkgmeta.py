01299658373cf7a727712a5229d6a0707fa41148
Chris McKenzie <me@christophermckenzie.com>
2015-09-02 13:58:17 -0400

    1.0.0 release

--- combocache/pkgmeta.py
+++ combocache/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'django-combocache'
 __author__ = 'Chris McKenzie'
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 __license__ = 'MIT'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
